235599ab7c8f7a86da958c11a8a55dc9f22e4787
Andrew Masek <andrewmasek@gmail.com>
2023-05-18 02:05:03 -0700

    Added linear interpolation

--- 180sound.py
+++ 180sound.py
@@ -6,11 +6,17 @@ import time
 
 # This script moves a sound source in a circle around the listener
 
-INPUT_FILE = 'samples/tone.wav'
-OUTPUT_FILE = '180tone.wav'
+INPUT_FILE = 'samples/onandon.mp3'
+OUTPUT_FILE = 'onandon.wav'
 HRIR_FOLDER = 'kemardb\elev0'
 
-BUFFER_LENGTH = 11025 # Number of samples in buffer
+# Sample rate of input/ buffer length gives time for 5 degrees
+SAMPLE_RATE = 44100
+BUFFER_LENGTH = 350 # Number of samples in buffer
+SPATIALIZATION_JUMP = 0.05 # Amount to jump between discrete HRIRs in degrees
+SAMPLES_PER_JUMP = BUFFER_LENGTH // SAMPLE_RATE # Number of samples within a spatialization position
+NUM_SPATIALIZATION_POSITIONS = 180 // SPATIALIZATION_JUMP # Number of discrete spatialization positions
+
 
 # Get all HRIR filepaths
 HRIR_FILES = [file for file in os.listdir(HRIR_FOLDER)]
@@ -18,6 +24,7 @@ HRIR_FILES.sort()
 
 # Load input audio file
 input_audio, input_audio_sr = sf.read(INPUT_FILE)
+print(input_audio_sr)
 input_audio = np.array(input_audio)
 
 # Make input audio mono if it's stereo
@@ -39,11 +46,24 @@ for starting_sample in tqdm.tqdm(range(0, num_samples, BUFFER_LENGTH)):
     input_chunk = input_audio[starting_sample: starting_sample + BUFFER_LENGTH]
 
     # Find appropriate HRIR for this chunk
-    CHUNK_INDEX = starting_sample // BUFFER_LENGTH % len(HRIR_FILES)
-    HRIR_file = os.path.join(HRIR_FOLDER, HRIR_FILES[CHUNK_INDEX])
+    current_azimuth = ((starting_sample // BUFFER_LENGTH) % NUM_SPATIALIZATION_POSITIONS) * (SPATIALIZATION_JUMP)
+    #print(current_azimuth)
+    # If the azimuth is divisible by 5 use the HRIR, otherwise interpolate between the 2 nearest
+    if current_azimuth % 5 == 0:
+        HRIR_file = os.path.join(HRIR_FOLDER, HRIR_FILES[int(current_azimuth // 5)])
+        #print(os.path.join(HRIR_FOLDER, HRIR_FILES[int(current_azimuth // 5)]))
+        # Load HRIR file
+        HRIR, HRIR_sr = sf.read(HRIR_file)
+    else:
+        first_HRIR_file = os.path.join(HRIR_FOLDER, HRIR_FILES[int(current_azimuth // 5)])
+        second_HRIR_file = os.path.join(HRIR_FOLDER, HRIR_FILES[int(current_azimuth // 5) + 1])
 
-    # Load HRIR file
-    HRIR, HRIR_sr = sf.read(HRIR_file)
+        first_HRIR, first_HRIR_sr = sf.read(first_HRIR_file)
+        second_HRIR, second_HRIR_sr = sf.read(second_HRIR_file)
+
+        first_weight, second_weight = (5 - abs(current_azimuth - (current_azimuth // 5) * 5)), (5 - abs(current_azimuth - (((current_azimuth // 5) + 1) * 5)))
+        
+        HRIR = (first_HRIR * first_weight + second_HRIR * second_weight)/5
 
     # Find padding length needed to bring each convolution result to 2 * BUFFER_LENGTH size
     padding_amount = 2 * BUFFER_LENGTH - (len(input_chunk) + len(HRIR[:, 0]) - 1)
